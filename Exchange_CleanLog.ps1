41c2fd7039c78e25e70aa3d32c8d4d102328042b
Christophe Pelichet <c.pelichet@gmail.com>
2019-02-07 20:01:22 +0100

    - Add "<myscript>\Conf" directory  - Add "<myscript>\Conf\config.xml" files -  Add "<myscript>\Log" directory -  Add "<myscript>\Functions" directory -  Add "<myscript>\Functions\Get_Load_Config.ps1" function - rewrite code

--- Exchange_CleanLog.ps1
+++ Exchange_CleanLog.ps1
@@ -1,7 +1,110 @@
-Set-Executionpolicy RemoteSigned
+#  ███████╗██╗  ██╗ ██████╗██╗  ██╗ █████╗ ███╗   ██╗ ██████╗ ███████╗         ██████╗██╗     ███████╗ █████╗ ███╗   ██╗        ██╗      ██████╗  ██████╗ 
+#  ██╔════╝╚██╗██╔╝██╔════╝██║  ██║██╔══██╗████╗  ██║██╔════╝ ██╔════╝        ██╔════╝██║     ██╔════╝██╔══██╗████╗  ██║        ██║     ██╔═══██╗██╔════╝ 
+#  █████╗   ╚███╔╝ ██║     ███████║███████║██╔██╗ ██║██║  ███╗█████╗          ██║     ██║     █████╗  ███████║██╔██╗ ██║        ██║     ██║   ██║██║  ███╗
+#  ██╔══╝   ██╔██╗ ██║     ██╔══██║██╔══██║██║╚██╗██║██║   ██║██╔══╝          ██║     ██║     ██╔══╝  ██╔══██║██║╚██╗██║        ██║     ██║   ██║██║   ██║
+#  ███████╗██╔╝ ██╗╚██████╗██║  ██║██║  ██║██║ ╚████║╚██████╔╝███████╗███████╗╚██████╗███████╗███████╗██║  ██║██║ ╚████║███████╗███████╗╚██████╔╝╚██████╔╝
+#  ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝ ╚═════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝╚══════╝ ╚═════╝  ╚═════╝ 
+#                                                                                                                                                         
+<#
 
-$days=7 #You can change the number of days here
-$IISLogPath="C:\inetpub\logs\LogFiles\"
+.SYNOPSIS 
+
+	
+.DESCRIPTION
+
+	
+.EXAMPLE
+
+
+.LINK
+	
+
+.NOTES
+
+Written by : Christophe Pelichet
+Version:     1.0
+
+Find me on : 
+
+	* LinkedIn:	www.linkedin.com/in/christophe-pelichet-868bb644
+	* Github:	https://github.com/christophepelichet
+
+#>
+
+
+
+
+
+
+
+
+
+
+##############################################################################################################################################################
+################################################################## SCRIPT PATH ###############################################################################
+##############################################################################################################################################################
+
+$MyScriptPath 		=  Split-Path $script:MyInvocation.MyCommand.Path								# Path du Script
+$MyScriptFunctions	= "$MyScriptPath\Functions"														# Path du répertoire des fonctions
+$MyScriptLog		= "$MyScriptPath\Log"															# Path du répertoire de logs
+$MyScriptConfdir	= "$MyScriptPath\Conf"															# Path du répertoire de configuration
+$MyScriptConfname 	= "config.xml"																	# Nom du fichier de configuration
+
+
+##############################################################################################################################################################
+#################################################################  GET_LOAD_CONFIG  ##########################################################################
+##############################################################################################################################################################
+
+### Concatenation du path du fichier de configuration
+$MyScriptConfig   = $MyScriptConfdir+"\"+ $MyScriptConfname											
+
+### Chargement de la fonction Get_LoadConfig et du fichier de configuration
+$MyLoadConf = $MyScriptFunctions +"\"+ "Get_LoadConfig.ps1"											
+	. $MyLoadConf																					
+	Get_LoadConfig -path $MyScriptConfig	
+
+
+##############################################################################################################################################################
+################################################################ PATH CONCATENATION ##########################################################################
+##############################################################################################################################################################	
+	
+##############################################################################################################################################################
+############################################################# CONFIG FILE TO VARIABLES #######################################################################
+##############################################################################################################################################################	
+
+### Retention
+$RetentionDay 	 = $appSettings["RetentionDay"]							 						# Envoie du rapport par email :   1 activé / 0 désactivé
+
+
+	
+###############################################################################################################################################################
+##################################################### MERCI DE NE RIEN MODIFIER APRES CETTE LIGNE ############################################################# 
+###############################################################################################################################################################
+
+
+# Control que IIS est Installé
+
+if ((Get-WindowsFeature Web-Server).InstallState -eq "Installed") {
+	# Clear Variables
+	$WWWSite 		= $NULL
+	$WWWSiteLogDir	= $NULL
+	
+	# Get Web Site Log Dir Path
+	$WWWSite 		= Get-website -name "Default Web Site" | Select logFile
+	$WWWSiteLogDir 	= $WWWSite.LogFile.Directory -Replace '%SystemDrive%', $env:SystemDrive
+} 
+else 
+{
+    Write-Host "IIS is not installed on $vm"
+}
+
+
+
+
+
+
+
+<# S
 $ExchangeLoggingPath="C:\Program Files\Microsoft\Exchange Server\V15\Logging\"
 
 Write-Host "Removing IIS and Exchange logs; keeping last" $days "days"
@@ -20,4 +123,4 @@ Else {
     }
 }
 CleanLogfiles($IISLogPath)
-CleanLogfiles($ExchangeLoggingPath)
+CleanLogfiles($ExchangeLoggingPath) #>
---
+++ Functions/Get_LoadConfig.ps1
@@ -0,0 +1,76 @@
+﻿
+#   ██████╗██╗  ██╗██████╗ ██╗███████╗████████╗ ██████╗ ██████╗ ██╗  ██╗███████╗    ██████╗ ███████╗██╗     ██╗ ██████╗██╗  ██╗███████╗████████╗
+#  ██╔════╝██║  ██║██╔══██╗██║██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██║  ██║██╔════╝    ██╔══██╗██╔════╝██║     ██║██╔════╝██║  ██║██╔════╝╚══██╔══╝
+#  ██║     ███████║██████╔╝██║███████╗   ██║   ██║   ██║██████╔╝███████║█████╗      ██████╔╝█████╗  ██║     ██║██║     ███████║█████╗     ██║   
+#  ██║     ██╔══██║██╔══██╗██║╚════██║   ██║   ██║   ██║██╔═══╝ ██╔══██║██╔══╝      ██╔═══╝ ██╔══╝  ██║     ██║██║     ██╔══██║██╔══╝     ██║   
+#  ╚██████╗██║  ██║██║  ██║██║███████║   ██║   ╚██████╔╝██║     ██║  ██║███████╗    ██║     ███████╗███████╗██║╚██████╗██║  ██║███████╗   ██║   
+#   ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝     ╚═╝  ╚═╝╚══════╝    ╚═╝     ╚══════╝╚══════╝╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝   ╚═╝   
+#   ██████╗ ███████╗████████╗        ██╗      ██████╗  █████╗ ██████╗          ██████╗ ██████╗ ███╗   ██╗███████╗██╗ ██████╗                    
+#  ██╔════╝ ██╔════╝╚══██╔══╝        ██║     ██╔═══██╗██╔══██╗██╔══██╗        ██╔════╝██╔═══██╗████╗  ██║██╔════╝██║██╔════╝                    
+#  ██║  ███╗█████╗     ██║           ██║     ██║   ██║███████║██║  ██║        ██║     ██║   ██║██╔██╗ ██║█████╗  ██║██║  ███╗                   
+#  ██║   ██║██╔══╝     ██║           ██║     ██║   ██║██╔══██║██║  ██║        ██║     ██║   ██║██║╚██╗██║██╔══╝  ██║██║   ██║                   
+#  ╚██████╔╝███████╗   ██║   ███████╗███████╗╚██████╔╝██║  ██║██████╔╝███████╗╚██████╗╚██████╔╝██║ ╚████║██║     ██║╚██████╔╝                   
+#   ╚═════╝ ╚══════╝   ╚═╝   ╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝     ╚═╝ ╚═════╝                    
+#                                                                                                                                               
+
+Function Get_LoadConfig () 
+{
+
+Param 
+	(
+		[Parameter(Mandatory=$true)][string]$Path
+	)
+
+<#
+
+.SYNOPSIS 
+	Fonction pour charger un fichier de configuration au format .xml
+	Function to load a configuration file in .xml format
+	
+.DESCRIPTION
+	Fonction pour charger un fichier de configuration au format .xml
+	Function to load a configuration file in .xml format
+	
+.EXAMPLE
+	Get_LoadConfig -path <config_xml_path>
+
+.LINK
+	
+
+.NOTES
+
+Written by : Christophe Pelichet
+Version:     1.0
+
+Find me on : 
+
+	* LinkedIn:	www.linkedin.com/in/christophe-pelichet-868bb644
+	* Github:	https://github.com/christophepelichet
+
+#>
+
+
+	
+
+
+	$global:appSettings = @{}
+	$config = [xml](get-content $path)
+
+		foreach ($addNode in $config.configuration.appsettings.add) {
+			if ($addNode.Value.Contains(‘,’)) 
+			{
+			# Array case
+				$value = $addNode.Value.Split(‘,’)
+				for ($i = 0; $i -lt $value.length; $i++) 
+				{ 
+				$value[$i] = $value[$i].Trim() 
+				}
+			}
+			else 
+			{
+			# Scalar case
+				$value = $addNode.Value
+			}
+				$global:appSettings[$addNode.Key] = $value
+		}
+}
